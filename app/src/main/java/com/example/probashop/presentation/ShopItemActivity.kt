c6d3a9d54af3611bd3400bae3cb70435fe62e5c1
Grib_tema <grib_tema@gitlab.com>
2022-11-02 00:43:59 +0300

    Add shop item activity and resize window in input mode

---
+++ app/src/main/java/com/example/probashop/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package com.example.probashop.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.probashop.R
+
+class ShopItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
