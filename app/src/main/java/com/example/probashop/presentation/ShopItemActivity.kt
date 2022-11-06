1df0b9335a45a22d3ce653deb0fce54b0d9baf9a
Grib_tema <grib_tema@gitlab.com>
2022-11-06 19:38:46 +0300

    Init Views and parse intent for ShopItemActivity

--- app/src/main/java/com/example/probashop/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/probashop/presentation/ShopItemActivity.kt
@@ -3,16 +3,70 @@ package com.example.probashop.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.probashop.R
+import com.example.probashop.domain.ShopItem
+import com.google.android.material.textfield.TextInputLayout
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etCount: EditText
+    private lateinit var buttonSave: Button
+
+    private var screenMode = UNKNOWN_MODE
+    private var shopItemId = ShopItem.UNDEFINED_ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        parseIntent()
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        initViews()
+        when (screenMode) {
+            EDIT_MODE -> launchEditMode()
+            ADD_MODE -> launchAddMode()
+        }
+    }
+
+    private fun launchEditMode() {
+        // TODO: create fun
+    }
+
+    private fun launchAddMode() {
+        // TODO: create fun
+    }
+
+    private fun parseIntent() {
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Param screen mode is absent")
+        }
         val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-        Log.d("ShopItemActivity", mode.toString())
+        if ((mode != ADD_MODE) && (mode != EDIT_MODE)) {
+            throw RuntimeException("Unknown screen mode: $mode")
+        }
+        screenMode = mode
+        if (screenMode == EDIT_MODE) {
+            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+                throw RuntimeException("Param shop item id is absent")
+            }
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    private fun initViews() {
+        tilName = findViewById(R.id.til_name)
+        tilCount = findViewById(R.id.til_count)
+        etName = findViewById(R.id.et_name)
+        etCount = findViewById(R.id.et_count)
+        buttonSave = findViewById(R.id.save_button)
     }
 
     companion object {
@@ -20,6 +74,7 @@ class ShopItemActivity : AppCompatActivity() {
         private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
         private const val ADD_MODE = "add_mode"
         private const val EDIT_MODE = "edit_mode"
+        private const val UNKNOWN_MODE = ""
 
         fun newShopItemIntent(context: Context): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
